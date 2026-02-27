f6aa7417b0300ff2ed8ac4a58137d7de1e0727a8
satnandevi250127 <satnandevi250127@gmail.com>
2026-02-27 07:20:46 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,15 @@
+let currentSlide = 0;
+const slider = document.getElementById('slider');
+const totalSlides = slider.children.length;
+
+function autoSlide() {
+    currentSlide++;
+
+    if (currentSlide >= totalSlides) {
+        currentSlide = 0;
+    }
+
+    slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+setInterval(autoSlide, 3000);
